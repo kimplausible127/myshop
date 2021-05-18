6b5b25bde9382913c5ebe394801ccd675d310572
Andy Kim <kimplausible127@gmail.com>
2021-05-18 08:41:17 -0700

    Added catalog views for products with url mappings

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Category, Product
+
+
+def product_list(request, category_slug=None):
+    categories = Category.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category,slug=category_slug)
+        products = Product.objects.filter(category=category)
+    else:
+        category = None
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product})
+                  
+
