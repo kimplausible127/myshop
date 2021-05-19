d0a90fc33fd244b4fbd48dc541896d7443efa6e2
Andy Kim <kimplausible127@gmail.com>
2021-05-18 20:02:35 -0700

    Added view to add,update, remove, and display cart items and totals. Built template to display cart. Added CartAddProductForm.

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
+from cart.forms import CartAddProductForm
 from .models import Category, Product
 
 
@@ -23,8 +24,10 @@ def product_detail(request, id, slug):
                                 id=id,
                                 slug=slug,
                                 available=True)
+    cart_product_form = CartAddProductForm()
     return render(request,
                   'shop/product/detail.html',
-                  {'product': product})
+                  {'product': product,
+                   'cart_product_form': cart_product_form})
 
 
